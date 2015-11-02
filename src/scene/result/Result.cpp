b4405ca633e898b8dac50aabf3534bb57cb95b7c
Lacty <akira206@gmail.com>
2015-11-03 06:09:10 +0900

    result追加

--- src/scene/result/Result.cpp
+++ src/scene/result/Result.cpp
@@ -1,18 +1,18 @@
 
-#include "GameMain.hpp"
+#include "Result.hpp"
 #include "cinder/gl/gl.h"
 
 
-GameMain::GameMain() {}
+Result::Result() {}
 
-void GameMain::update() {
-  setNextScene(SceneType::Result);
+void Result::update() {
+  
 }
 
-void GameMain::draw() {
+void Result::draw() {
   ci::gl::pushModelView();
   ci::gl::translate(ci::app::getWindowCenter());
-  ci::gl::color(ci::Color(1, 0, 0));
+  ci::gl::color(ci::Color(0, 1, 0));
   ci::gl::drawCube(ci::Vec3f::zero(), ci::Vec3f(100, 100, 100));
   ci::gl::popModelView();
 }
