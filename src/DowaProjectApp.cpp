f2fb312374578990cab94a08273813ab65cecbc4
Lacty <akira206@gmail.com>
2015-11-02 16:27:25 +0900

    動作確認完了

--- src/DowaProjectApp.cpp
+++ src/DowaProjectApp.cpp
@@ -1,34 +1,32 @@
+
 #include "cinder/app/AppNative.h"
 #include "cinder/gl/gl.h"
 
 using namespace ci;
 using namespace ci::app;
-using namespace std;
 
 class DowaProjectApp : public AppNative {
-  public:
-	void setup();
-	void mouseDown( MouseEvent event );	
-	void update();
-	void draw();
+public:
+  void setup();
+  void mouseDown(MouseEvent event);
+  void update();
+  void draw();
 };
 
-void DowaProjectApp::setup()
-{
-}
+void DowaProjectApp::setup() {}
 
-void DowaProjectApp::mouseDown( MouseEvent event )
-{
-}
+void DowaProjectApp::mouseDown(MouseEvent event) {}
 
-void DowaProjectApp::update()
-{
-}
+void DowaProjectApp::update() {}
 
-void DowaProjectApp::draw()
-{
-	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+void DowaProjectApp::draw() {
+	gl::clear(Color(0.4f, 0.4f, 0.4f));
+  
+  gl::pushModelView();
+  gl::translate(getWindowCenter());
+  gl::color(Color(1, 1, 1));
+  gl::drawCube(Vec3f::zero(), Vec3f(100, 100, 100));
+  gl::popModelView();
 }
 
 CINDER_APP_NATIVE( DowaProjectApp, RendererGl )
