258500bfa77f92eb050ca4bcfcd33cf8aa3b3c26
Lacty <akira206@gmail.com>
2015-11-08 19:40:33 +0900

    device.cpp不必要な処理を消した

---
+++ src/object/Object.cpp
@@ -0,0 +1,9 @@
+//
+//  Object.cpp
+//  DowaProject
+//
+//  Created by Yanai on 2015/11/08.
+//
+//
+
+#include "Object.hpp"
---
+++ src/object/Object.hpp
@@ -0,0 +1,14 @@
+//
+//  Object.hpp
+//  DowaProject
+//
+//  Created by Yanai on 2015/11/08.
+//
+//
+
+#ifndef Object_hpp
+#define Object_hpp
+
+#include <stdio.h>
+
+#endif /* Object_hpp */
