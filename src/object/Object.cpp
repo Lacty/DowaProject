bc036e094ca82fa962b1b07928c78c47d885e9ab
Lacty <akira206@gmail.com>
2015-11-08 22:48:04 +0900

    Object追加

--- src/object/Object.cpp
+++ src/object/Object.cpp
@@ -1,9 +1,55 @@
-//
-//  Object.cpp
-//  DowaProject
-//
-//  Created by Yanai on 2015/11/08.
-//
-//
 
 #include "Object.hpp"
+
+
+Object::Object() :
+mState(State::Awake),
+mCollider(Collider::None) {}
+
+void Object::setState(const State& state) {
+  mState = state;
+}
+
+const Object::State Object::getState() const {
+  return mState;
+}
+
+const bool Object::isAwake() const {
+  return mState == State::Awake;
+}
+
+const bool Object::isActive() const {
+  return mState == State::Active;
+}
+
+const bool Object::isDead() const {
+  return mState == State::Dead;
+}
+
+void Object::setColliderType(const Collider& type) {
+  mCollider = type;
+}
+
+const Object::Collider Object::getColliderType() const {
+  return mCollider;
+}
+
+const bool Object::isColliderTypeNone() const {
+  return mCollider == Collider::None;
+}
+
+const bool Object::isColliderTypeRect() const {
+  return mCollider == Collider::Rect;
+}
+
+const bool Object::isColliderTypeCircle() const {
+  return mCollider == Collider::Circle;
+}
+
+void Object::setName(const std::string& name) {
+  mName = name;
+}
+
+const std::string Object::getName() const {
+  return mName;
+}
--- src/object/Object.hpp
+++ src/object/Object.hpp
@@ -1,14 +1,48 @@
-//
-//  Object.hpp
-//  DowaProject
-//
-//  Created by Yanai on 2015/11/08.
-//
-//
 
-#ifndef Object_hpp
-#define Object_hpp
+#pragma once
+#include <string>
 
-#include <stdio.h>
 
-#endif /* Object_hpp */
+class Object {
+public:
+  enum class State {
+    Awake,
+    Active,
+    Dead
+  };
+  
+  enum class Collider {
+    None,
+    Rect,
+    Circle
+  };
+  
+  Object();
+  
+  void setState(const State& state);
+  const State getState() const;
+  
+  const bool isAwake() const;
+  const bool isActive() const;
+  const bool isDead() const;
+  
+  void setColliderType(const Collider& type);
+  const Collider getColliderType() const;
+  
+  const bool isColliderTypeNone() const;
+  const bool isColliderTypeRect() const;
+  const bool isColliderTypeCircle() const;
+  
+  void setName(const std::string& name);
+  const std::string getName() const;
+  
+  virtual void setup() = 0;
+  virtual void update() = 0;
+  virtual void draw() = 0;
+  virtual void onCollisionUpdate() {};
+  
+protected:
+  std::string mName;
+  State mState;
+  Collider mCollider;
+};
