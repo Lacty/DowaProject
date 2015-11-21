e15de7d3e5cb478c6dfed33eec6543efe63dd0bf
PS14 <phantasystar2115@yahoo.co.jp>
2015-11-22 02:44:55 +0900

    リザルト画面
    result

--- src/scene/result/Result.hpp
+++ src/scene/result/Result.hpp
@@ -3,9 +3,26 @@
 #include "../Scene.hpp"
 
 
-class Result : public Scene {
+
+
+class Result : public Scene 
+{
 private:
-  
+	int mChange;
+	int mCount;
+	float mr;
+	
+	
+
+	enum mChange
+	{
+		mScreen1,
+		mScreen2,
+		mScreen3,
+		mScreen4,
+		mScreen5,
+		mScreen6
+	};
 public:
   Result();
   
