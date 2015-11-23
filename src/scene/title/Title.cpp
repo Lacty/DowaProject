fa6ab3c70f5b7320dacbbd1223a3c1c632814f5d
pedius1175 <newdiamondsong1175@gmail.com>
2015-11-23 16:14:30 +0900

    Update Title.cpp
    完全修正版、唐揚げ食べたい

--- src/scene/title/Title.cpp
+++ src/scene/title/Title.cpp
@@ -1,10 +1,15 @@
 
 
-// iPad画面サイズ
-// iPhone画面サイズ
-// 注意！横画面にしてもxとyの座標は変わらない可能性あり
-// どうしてもうまくいかない場合はxとyを逆にしてみること
-// デバッグ用cpp リリースのときは無駄な文を消すこと iostream、usingなど
+/*
+ 
+ iPad画面サイズ 768 × 1024
+ iPhone画面サイズ 320 × 568
+ 注意！横画面にしてもxとyの座標は変わらない可能性あり
+ どうしてもうまくいかない場合はxとyを逆にしてみること
+ ビルドするときの注意事項 Deployment Info -> Device Orientation
+ Landscape Left,Landscape Right にチェックを入れること、それ以外はチェックを外す
+ 
+ */
 
 #include "Title.hpp"
 #include "cinder/app/AppNative.h"
@@ -12,14 +17,17 @@
 #include "cinder/gl/Texture.h"
 #include "cinder/ImageIo.h"
 #include <math.h>
-#include <iostream> // Debug
-
-using namespace std; // Debug
+#include "../../device/Device.hpp"
+#include "../../scene/SceneManager.hpp"
 
 Title::Title()
 {
-<<<<<<< HEAD
-  //Load Textures
+  
+  // デバイスのウィンドサイズを取得し、代入
+  mDeviceWindowHeight = ci::app::getWindowHeight();
+  mDeviceWindowWidth = ci::app::getWindowWidth();
+  
+  // 画像読み込み
   dowa::ResourceManager::texture().insert("background/title/TitleBack.jpg", TextureKey::TitleBack);
   dowa::ResourceManager::texture().insert("background/title/FeatherBookResize.png", TextureKey::FeatherBook);
   dowa::ResourceManager::texture().insert("background/title/Logo.png", TextureKey::Logo);
@@ -32,82 +40,68 @@ Title::Title()
   dowa::ResourceManager::texture().insert("background/title/SphereBlue.png", TextureKey::SphereBlue);
   dowa::ResourceManager::texture().insert("background/title/SphereGreen.png", TextureKey::SphereGreen);
   dowa::ResourceManager::texture().insert("background/title/SphereRed.png", TextureKey::SphereRed);
-
-=======
   
->>>>>>> 2f0ed8ca6208f6b160ca95c05a623210ed434013
-  // 背景
-  //mTitleBack = (loadImage(cinder::app::loadAsset("background/title/TitleBack.jpg")));
-
-  // 本とペン 「描画する画像が大きすぎてちっちゃく描画したときにジャギが発生していた為、元画像を 912 × 768 にリサイズ」
-  //mFeatherBook = (loadImage(cinder::app::loadAsset("background/title/FeatherBook.png")));
-  mFeatherBookRect = ci::Rectf(150, 15, 430, 250); // size = 280, 235
   
-  // スカーフみたいなやつ
-  //mTrace = (loadImage(cinder::app::loadAsset("background/title/Trace.png")));
-  mTraceRect = ci::Rectf(120, 50, 340, 245); // size = 220, 195
-
-
-  // 文字「た」
-  //mKanaLetterTa = (loadImage(cinder::app::loadAsset("background/title/KanaLetterTa.png")));
-  mKanaLetterTaRect = ci::Rectf(130, 80, 220, 170); // size = 90, 90
-  // 文字「ま」
-  //mKanaLetterMa = (loadImage(cinder::app::loadAsset("background/title/KanaLetterMa.png")));
-  mKanaLetterMaRect = ci::Rectf(210, 15, 300, 105); // size = 90, 90
-  // 文字「こ」
-  //mKanaLetterKo = (loadImage(cinder::app::loadAsset("background/title/KanaLetterKo.png")));
-  mKanaLetterKoRect = ci::Rectf(260, 100, 350, 190); // size = 90, 90
-  // 文字「ろ」
-  //mKanaLetterRo = (loadImage(cinder::app::loadAsset("background/title/KanaLetterRo.png")));
-  mKanaLetterRoRect = ci::Rectf(350, 40, 440, 130); // size = 90, 90
-
-
-  // ボール青
-  //mSphereBlue = (loadImage(cinder::app::loadAsset("background/title/SphereBlue.png")));
-  mSphereBlueRect = ci::Rectf(320, 25, 360, 65); // size = 40, 40
-  // ボール緑
-  //mSphereGreen = (loadImage(cinder::app::loadAsset("background/title/SphereGreen.png")));
-  mSphereGreenRect = ci::Rectf(130, 165, 180, 215); // size = 50, 50
-  // ボール赤
-  //mSphereRed = (loadImage(cinder::app::loadAsset("background/title/SphereRed.png")));
-  mSphereRedRect = ci::Rectf(350, 173, 420, 243); // size = 70, 70
-
-
-  // 文字「TAMACORO」
-  //mLogo = (loadImage(cinder::app::loadAsset("background/title/Logo.png")));
-  mLogoRect = ci::Rectf(190, 200, 280, 220); // size = 90, 20
-  // 文字「タッチしてスタート！」
-  //mTapToStartEng = (loadImage(cinder::app::loadAsset("background/title/TapToStartEng.png")));
-  mTapToStartEngRect = ci::Rectf(180, 265, 400, 295); // size = 220, 30
-
+  // 画像座標設定
+  mFeatherBookRect = ci::Rectf(mDeviceWindowHeight / 5,  mDeviceWindowWidth / 20,
+                               mDeviceWindowHeight / 1.2, mDeviceWindowWidth / 1.28); // 本とペン
+  
+  mTraceRect = ci::Rectf(mDeviceWindowHeight / 4.7, mDeviceWindowWidth / 6.5,
+                         mDeviceWindowHeight / 1.5, mDeviceWindowWidth / 1.3); // 羽
+  
+  mLogoRect = ci::Rectf(mDeviceWindowHeight / 3.5, mDeviceWindowWidth / 1.6,
+                        mDeviceWindowHeight / 2, mDeviceWindowWidth / 1.45); // TAMACORO
+  
+  mTapToStartEngRect = ci::Rectf(mDeviceWindowHeight / 3.3, mDeviceWindowWidth / 1.22,
+                                 mDeviceWindowHeight / 1.35, mDeviceWindowWidth / 1.12); // タッチしてスタート！
 }
 
 void Title::update()
 {
+  
+  if(dowa::Device::isTouchBegan()) // タッチされたらシーン切り替え
+  {
+    SceneManager::create(SceneType::Result); // とりあえずリザルトに切り替え
+  }
 
   mSphereMotion++;
 
 
-  mKanaLetterTaRect = ci::Rectf(130, 80 + sin(M_PI * 2 / 240 * mSphereMotion) * 2,
-    220, 170 + sin(M_PI * 2 / 240 * mSphereMotion) * 2);
+  mKanaLetterTaRect = ci::Rectf(mDeviceWindowHeight / 4.5,
+                                mDeviceWindowWidth / 4 + sin(M_PI * 2 / 240 * mSphereMotion) * 2,
+                                mDeviceWindowHeight / 2.5,
+                                mDeviceWindowWidth / 2 + sin(M_PI * 2 / 240 * mSphereMotion) * 2); // た
+  
+  mKanaLetterMaRect = ci::Rectf(mDeviceWindowHeight / 2.7,
+                                mDeviceWindowWidth / 10 + sin(M_PI * 2 / 240 * mSphereMotion) * -2,
+                                mDeviceWindowHeight / 1.77,
+                                mDeviceWindowWidth / 3 + sin(M_PI * 2 / 240 * mSphereMotion) * -2); // ま
 
-  mKanaLetterMaRect = ci::Rectf(210, 15 + sin(M_PI * 2 / 240 * mSphereMotion) * 4,
-    300, 105 + sin(M_PI * 2 / 240 * mSphereMotion) * 4);
+  mKanaLetterKoRect = ci::Rectf(mDeviceWindowHeight / 2.1,
+                                mDeviceWindowWidth / 3.1 + sin(M_PI * 2 / 240 * mSphereMotion) * 3,
+                                mDeviceWindowHeight / 1.5,
+                                mDeviceWindowWidth / 1.8 + sin(M_PI * 2 / 240 * mSphereMotion) * 3); // こ
 
-  mKanaLetterKoRect = ci::Rectf(260, 100 + sin(M_PI * 2 / 240 * mSphereMotion) * -2,
-    350, 190 + sin(M_PI * 2 / 240 * mSphereMotion) * -2);
-
-  mKanaLetterRoRect = ci::Rectf(350, 40 + sin(M_PI * 2 / 240 * mSphereMotion) * -3,
-    440, 130 + sin(M_PI * 2 / 240 * mSphereMotion) * -3);
-
-  mSphereBlueRect = ci::Rectf(320, 25 + sin(M_PI * 2 / 240 * mSphereMotion) * 10,
-    360, 65 + sin(M_PI * 2 / 240 * mSphereMotion) * 10);
-
-  mSphereGreenRect = ci::Rectf(130, 165 + sin(M_PI * 2 / 240 * mSphereMotion) * -3,
-    180, 215 + sin(M_PI * 2 / 240 * mSphereMotion) * -3);
-
-  mSphereRedRect = ci::Rectf(350, 173 + sin(M_PI * 2 / 240 * mSphereMotion) * 7,
-    420, 243 + sin(M_PI * 2 / 240 * mSphereMotion) * 7);
+  mKanaLetterRoRect = ci::Rectf(mDeviceWindowHeight / 1.6,
+                                mDeviceWindowWidth / 6 + sin(M_PI * 2 / 240 * mSphereMotion) * 1.5,
+                                mDeviceWindowHeight / 1.22,
+                                mDeviceWindowWidth / 2.5 + sin(M_PI * 2 / 240 * mSphereMotion) * 1.5); // ろ
+  
+  mSphereBlueRect = ci::Rectf(mDeviceWindowHeight / 1.8,
+                              mDeviceWindowWidth / 7.9 + sin(M_PI * 2 / 240 * mSphereMotion) * 1.5,
+                              mDeviceWindowHeight / 1.55,
+                              mDeviceWindowWidth / 4.1 + sin(M_PI * 2 / 240 * mSphereMotion) * 1.5); // 青
+  
+  mSphereGreenRect = ci::Rectf(mDeviceWindowHeight / 5.8,
+                               mDeviceWindowWidth / 2.0 + sin(M_PI * 2 / 240 * mSphereMotion) * -1.5,
+                               mDeviceWindowHeight / 3.28,
+                               mDeviceWindowWidth / 1.5 + sin(M_PI * 2 / 240 * mSphereMotion) * -1.5); // 緑
+  
+  mSphereRedRect = ci::Rectf(mDeviceWindowHeight / 1.6,
+                             mDeviceWindowWidth / 1.8 + sin(M_PI * 2 / 240 * mSphereMotion) * -2.5,
+                             mDeviceWindowHeight / 1.27,
+                             mDeviceWindowWidth  / 1.3 + sin(M_PI * 2 / 240 * mSphereMotion) * -2.5); // 赤
+  
 }
 
 void Title::draw()
@@ -120,44 +114,25 @@ void Title::draw()
   ci::gl::clear();
   
   cinder::gl::enableAlphaBlending(true); // αチャンネル有効
-<<<<<<< HEAD
 
-  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::TitleBack), ci::app::getWindowBounds());
-  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::FeatherBook), mFeatherBookRect);
-  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::Trace), mTraceRect);
+  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::TitleBack), ci::app::getWindowBounds()); // 背景
+  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::FeatherBook), mFeatherBookRect); // 本とペン
+  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::Trace), mTraceRect); // 羽
 
-  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::LetterTa), mKanaLetterTaRect);
-  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::LetterMa), mKanaLetterMaRect);
-  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::LetterKo), mKanaLetterKoRect);
-  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::LetterRo), mKanaLetterRoRect);
+  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::LetterTa), mKanaLetterTaRect); // た
+  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::LetterMa), mKanaLetterMaRect); // ま
+  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::LetterKo), mKanaLetterKoRect); // こ
+  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::LetterRo), mKanaLetterRoRect); // ろ
 
-  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::SphereBlue), mSphereBlueRect);
-  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::SphereGreen), mSphereGreenRect);
-  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::SphereRed), mSphereRedRect);
+  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::SphereBlue), mSphereBlueRect); // 青
+  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::SphereGreen), mSphereGreenRect); // 緑
+  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::SphereRed), mSphereRedRect); // 赤
 
-  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::Logo), mLogoRect);
-  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::TapToStartEng), mTapToStartEngRect);
-
-=======
-  
-  ci::gl::draw( mTitleBack, ci::app::getWindowBounds()); // 背景
-  ci::gl::draw( mFeatherBook, mFeatherBookRect); // 本とペン
-  ci::gl::draw( mTrace, mTraceRect); // スカーフみたいなやつ
-  
-  ci::gl::draw( mKanaLetterTa, mKanaLetterTaRect); // た
-  ci::gl::draw( mKanaLetterMa, mKanaLetterMaRect); // ま
-  ci::gl::draw( mKanaLetterKo, mKanaLetterKoRect); // こ
-  ci::gl::draw( mKanaLetterRo, mKanaLetterRoRect); // ろ
-  
-  ci::gl::draw( mSphereBlue, mSphereBlueRect); // ボール青
-  ci::gl::draw( mSphereGreen, mSphereGreenRect); // ボール緑
-  ci::gl::draw( mSphereRed, mSphereRedRect); // ボール赤
-  
-  ci::gl::draw( mLogo, mLogoRect); // TAMACORO
-  ci::gl::draw( mTapToStartEng, mTapToStartEngRect); // タッチしてスタート！
+  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::Logo), mLogoRect); // TAMACORO
+  ci::gl::draw(dowa::ResourceManager::texture().get(TextureKey::TapToStartEng), mTapToStartEngRect); // タッチしてスタート！
   
   cinder::gl::enableAlphaBlending(false); // αチャンネル無効
     
->>>>>>> 2f0ed8ca6208f6b160ca95c05a623210ed434013
   ci::gl::popModelView();
+  
 }
