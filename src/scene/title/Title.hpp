1149c212e1c2ceed49f2ffec8d8129e6ecac8909
pedius1175 <newdiamondsong1175@gmail.com>
2015-11-23 16:13:36 +0900

    Update Title.hpp
    完全修正版、抜かりなし

--- src/scene/title/Title.hpp
+++ src/scene/title/Title.hpp
@@ -2,8 +2,6 @@
 #pragma once
 
 #include "../Scene.hpp"
-//#include "cinder/ImageIo.h"
-//#include "cinder/gl/Texture.h"
 #include "../../resource/ResourceManager.hpp"
 
 class Title : public Scene
@@ -19,45 +17,21 @@ public:
 private:
   
   int mSphereMotion; // ボール微動
+  float mDeviceWindowHeight, mDeviceWindowWidth; // デバイスウィンドサイズ
   
-  //// 背景
-  //ci::gl::Texture mTitleBack;
-  //
-  //// 本とペン
-  //ci::gl::Texture mFeatherBook;
   ci::Rectf mFeatherBookRect;
-  // スカーフみたいなやつ
-  //ci::gl::Texture mTrace;
   ci::Rectf mTraceRect;
   
-  // 文字「た」
-  //ci::gl::Texture mKanaLetterTa;
   ci::Rectf mKanaLetterTaRect;
-  // 文字「ま」
-  //ci::gl::Texture mKanaLetterMa;
   ci::Rectf mKanaLetterMaRect;
-  // 文字「こ」
-  //ci::gl::Texture mKanaLetterKo;
   ci::Rectf mKanaLetterKoRect;
-  // 文字「ろ」
-  //ci::gl::Texture mKanaLetterRo;
   ci::Rectf mKanaLetterRoRect;
   
-  // ボール青
-  //ci::gl::Texture mSphereBlue;
   ci::Rectf mSphereBlueRect;
-  // ボール緑
-  //ci::gl::Texture mSphereGreen;
   ci::Rectf mSphereGreenRect;
-  // ボール赤
-  //ci::gl::Texture mSphereRed;
   ci::Rectf mSphereRedRect;
   
-  // 文字「TAMACORO」
-  //ci::gl::Texture mLogo;
   ci::Rectf mLogoRect;
-  // 文字「タッチしてスタート！」
-  //ci::gl::Texture mTapToStartEng;
   ci::Rectf mTapToStartEngRect;
   
 };
