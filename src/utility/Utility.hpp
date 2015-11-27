b792f08bd3c4caace32bf7f35c8b1d427bc8a990
Lacty <akira206@gmail.com>
2015-11-27 18:42:08 +0900

    コメント追加

--- src/utility/Utility.hpp
+++ src/utility/Utility.hpp
@@ -3,6 +3,8 @@
 #include "cinder/Vector.h"
 
 
+// turncates the deciminal point [len] or less
 ci::Vec2f floorv2(const ci::Vec2f& in, int len);
 
+// turncates the deciminal point [len] or less
 double floor2(double in, int len);
