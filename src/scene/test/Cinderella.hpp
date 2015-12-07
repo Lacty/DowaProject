a50fda107fdf2770e9539ac195a8d68d69d7062d
pedius1175 <newdiamondsong1175@gmail.com>
2015-12-07 22:13:26 +0900

    Create Cinderella.hpp
    hppよーん

---
+++ src/scene/test/Cinderella.hpp
@@ -0,0 +1,33 @@
+
+# pragma once
+
+#include "../../object/Object.hpp"
+#include "cinder/gl/Texture.h"
+#include "cinder/ImageIo.h"
+
+class Cinderella : public Object
+{
+private:
+  
+  int mCount; // アニメーション
+  float mGravityPower;
+  float  mVy;
+  
+  std::string mFloorStr;
+  
+  ci::Vec3f mTest;
+  
+  ci::gl::Texture mCinderellaStatic;
+  ci::gl::Texture mCinderellaLeft;
+  ci::gl::Texture mCinderellaRight;
+  
+public:
+  
+  Cinderella(ci::Vec3f mCinderellaPos, ci::Vec3f mCinderellaSize);
+  
+  void setup();
+  void update();
+  void draw();
+  
+  void onCollisionUpdate(const std::shared_ptr<Object>& compare); // 当たってるとき呼ばれる関数
+};
