61502511fd2b6ec457490b3a035cc6069462fdd3
RoDoRiTos <rodoritos_17@i.softbank.jp>
2015-12-10 14:31:00 +0900

    Minor Tweaks -Cinderella Scene Background Rect Resized -Drawn Loading String on Loading Scene

--- src/scene/cinderella/CinderellaScene.cpp
+++ src/scene/cinderella/CinderellaScene.cpp
@@ -10,18 +10,18 @@ CinderellaScene::CinderellaScene() {
     mCameraTarget, ci::Vec3f::yAxis());
   mWindowWidth = ci::app::getWindowWidth();
   mWindowHeight = ci::app::getWindowHeight();
-  mBack1 = ci::Rectf(-150, tanf(M_PI / 6) * 200, 450, -tanf(M_PI / 6) * 200);
+  mBack1 = ci::Rectf(-200, tanf(M_PI / 6) * 200, 1000, -tanf(M_PI / 6) * 200);
   /*mBack2 = ci::Rectf(150,100, 300,-100);
   mBack3 = ci::Rectf(300,100,450,-100);*/
 }
 
 
 void CinderellaScene::update() {
-  mBack1 = ci::Rectf(-150, tanf(M_PI / 6) * 200, 450, -tanf(M_PI / 6) * 200);
+  mBack1 = ci::Rectf(-200, tanf(M_PI / 6) * 200, 1000, -tanf(M_PI / 6) * 200);
   /*mBack2 = ci::Rectf(150 + mXPos, 100, 300 + mXPos, -100);
   mBack3 = ci::Rectf(300 + mXPos, 100, 450 + mXPos, -100);*/
-  mCameraPos.x += 0.1f;
-  mCameraTarget.x += 0.1f;
+  mCameraPos.x += 0.05f;
+  mCameraTarget.x += 0.05f;
   camera.lookAt(mCameraPos, mCameraTarget, Vec3f::yAxis());
 }
 
