486a98d7e8122c225eab25a5bc99a9b205bdd613
RoDoRiTos <rodoritos_17@i.softbank.jp>
2015-12-10 14:20:25 +0900

    Background and Camera Fixed

--- src/scene/cinderella/CinderellaScene.cpp
+++ src/scene/cinderella/CinderellaScene.cpp
@@ -4,22 +4,25 @@
 
 CinderellaScene::CinderellaScene() {
   camera = dowa::Camera(60, 0.5, 300);
-  camera.lookAt(ci::Vec3f(0, 0, 200), 
-    ci::Vec3f::zero(), ci::Vec3f::yAxis());
+  mCameraPos = Vec3f(0.0f,0.0f, 200.0f);
+  mCameraTarget = Vec3f::zero();
+  camera.lookAt(mCameraPos, 
+    mCameraTarget, ci::Vec3f::yAxis());
   mWindowWidth = ci::app::getWindowWidth();
   mWindowHeight = ci::app::getWindowHeight();
-  mBack1 = ci::Rectf(-150,100,450,-100);
+  mBack1 = ci::Rectf(-150, tanf(M_PI / 6) * 200, 450, -tanf(M_PI / 6) * 200);
   /*mBack2 = ci::Rectf(150,100, 300,-100);
   mBack3 = ci::Rectf(300,100,450,-100);*/
-  mXPos = 0;
 }
 
 
 void CinderellaScene::update() {
-  mXPos -= 0.1f;
-  mBack1 = ci::Rectf(-150 + mXPos, 100, 450 + mXPos, -100);
+  mBack1 = ci::Rectf(-150, tanf(M_PI / 6) * 200, 450, -tanf(M_PI / 6) * 200);
   /*mBack2 = ci::Rectf(150 + mXPos, 100, 300 + mXPos, -100);
   mBack3 = ci::Rectf(300 + mXPos, 100, 450 + mXPos, -100);*/
+  mCameraPos.x += 0.1f;
+  mCameraTarget.x += 0.1f;
+  camera.lookAt(mCameraPos, mCameraTarget, Vec3f::yAxis());
 }
 
 void CinderellaScene::draw() {
