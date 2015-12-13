bf72b5d5e95c909355066ddf7524aa85c19b5e50
Lacty <akira206@gmail.com>
2015-12-13 17:59:34 +0900

    SelectLoad追加

--- src/scene/SceneMaker.cpp
+++ src/scene/SceneMaker.cpp
@@ -7,7 +7,10 @@
 #include "gameMain/GameMain.hpp"
 #include "result/Result.hpp"
 #include "test/Test.hpp"
+
 #include "loading/TitleLoad.hpp"
+#include "loading/SelectLoad.hpp"
+
 #include "cinderella/CinderellaScene.hpp"
 
 #include <functional>
@@ -17,14 +20,17 @@
 std::unique_ptr<Scene> SceneMaker::create(const SceneType& type) const {
   using namespace std;
   unordered_map<SceneType, function<unique_ptr<Scene>()>> list = {
-    { SceneType::Title,      []{ return make_unique<Title>();      } },
-	  { SceneType::Select,     []{ return make_unique<Select>();     } },
-    { SceneType::GameMain,   []{ return make_unique<GameMain>();   } },
-    { SceneType::Result,     []{ return make_unique<Result>();     } },
+    { SceneType::Title,           []{ return make_unique<Title>();           } },
+	  { SceneType::Select,          []{ return make_unique<Select>();          } },
+    { SceneType::GameMain,        []{ return make_unique<GameMain>();        } },
+    { SceneType::Result,          []{ return make_unique<Result>();          } },
     
-    { SceneType::Test,       []{ return make_unique<Test>();       } },
-    { SceneType::TitleLoad,  []{ return make_unique<TitleLoad>();    } },
-    { SceneType::Cinderella, []{ return make_unique<CinderellaScene>(); } }
+    { SceneType::Test,            []{ return make_unique<Test>();            } },
+    
+    { SceneType::TitleLoad,       []{ return make_unique<TitleLoad>();       } },
+    { SceneType::SelectLoad,      []{ return make_unique<SelectLoad>();      } },
+    
+    { SceneType::Cinderella,      []{ return make_unique<CinderellaScene>(); } }
   };
   
   return std::move((list.find(type)->second)());
