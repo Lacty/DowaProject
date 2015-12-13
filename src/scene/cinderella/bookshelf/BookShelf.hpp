f40de11f0a7efb9cd2a5a94b2073e7742e6be044
hiramasa <newdiamondsong1175@gmail.com>
2015-12-14 02:32:04 +0900

    Update BookShelf.hpp

--- src/scene/cinderella/bookshelf/BookShelf.hpp
+++ src/scene/cinderella/bookshelf/BookShelf.hpp
@@ -4,19 +4,16 @@
 #include "../../../object/Object.hpp"
 
 #include "cinder/gl/Texture.h"
-#include "cinder/ImageIo.h"
 
 class BookShelf : public Object
 {
 private:
   
-  ci::gl::Texture mBookShelf; // 本棚
-  ci::gl::Texture mBookPile; // 本4冊
+  ci::gl::Texture mBookShelf;
   
 public:
   
   BookShelf(const ci::Vec3f&, const ci::Vec3f&);
-//  ~BookShelf();
   
   void setup();
   void update();
