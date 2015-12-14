475ae9cb20d03405956f0418cd710a5e1154c3e0
Lacty <akira206@gmail.com>
2015-12-14 20:46:06 +0900

    test空にした

--- src/scene/test/Test.cpp
+++ src/scene/test/Test.cpp
@@ -1,27 +1,11 @@
 
 #include "Test.hpp"
-#include "../../object/Task.hpp"
-#include "../../device/Device.hpp"
-#include "../SceneManager.hpp"
-
-#include "ball/Ball.hpp"
 
 
-Test::Test() {
-  cam = dowa::Camera();
-  Task::add("Ball", std::make_shared<Ball>(ci::Vec3f::zero(), ci::Vec3f(50, 50, 5)));
-}
+Test::Test() {}
 
-Test::~Test() {
-  ci::gl::popMatrices();
-  std::cout << "a\n";
-}
+Test::~Test() {}
 
-void Test::update() {
-  if (dowa::Device::isTouchBegan()) {
-    Task::clear();
-    SceneManager::create(SceneType::Test);
-  }
-}
+void Test::update() {}
 
 void Test::draw() {}
--- src/scene/test/Test.hpp
+++ src/scene/test/Test.hpp
@@ -1,16 +1,13 @@
 
 #pragma once
 #include "../Scene.hpp"
-#include "../cinderella/camera/Camera.hpp"
 
 
 class Test : public Scene {
 private:
-  dowa::Camera cam;
   
 public:
   Test();
-  
   ~Test();
   
   void update();
