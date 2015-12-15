e31414b4ea461b2c627d3cb0b173a92b83f4f10a
hiramasa <newdiamondsong1175@gmail.com>
2015-12-15 20:47:48 +0900

    Update Book.cpp

--- src/scene/cinderella/book/Book.cpp
+++ src/scene/cinderella/book/Book.cpp
@@ -1,35 +1,57 @@
 
 #include "Book.hpp"
 
+#include "../../../object/Task.hpp"
 #include "../../../resource/ResourceManager.hpp"
 
-// 位置、サイズ、名前
 Book::Book(const ci::Vec3f& mBookPos, const ci::Vec3f& mBookSize, const std::string& mBookNamef)
 {
   mPos = mBookPos;
   mSize = mBookSize;
-  
-  // 引数の名前を代入
+  mRotate = ci::Vec3f(ci::Vec3f::zero());
   mBookName = mBookNamef;
   
-  // 本の名前初期化
+  mGravityPower = 0.01f;
+  mGravityPower2 = 0.08f;
+  mAcceleration = 0.f;
+  
   mBookOpenName = "BookOpen";
   mBookPileName = "BookPile";
-  mBookDroppedName = "BookDropped";
-  mBookBackName = "BookBack";
+  mBookSideName = "BookSide";
   
-  // Texture代入
-  mBookPile = dowa::ResourceManager::texture().get(CinderellaTextureKey::BookPile); // 本４冊
-  mBookBack = dowa::ResourceManager::texture().get(CinderellaTextureKey::BookBack); // 本縦
-  mBookDropped = dowa::ResourceManager::texture().get(CinderellaTextureKey::BookDropped); // 落下
-  mBookOpen = dowa::ResourceManager::texture().get(CinderellaTextureKey::BookOpen); // オープン状態
+  mFallFlag = false;
+  
+  mCinderellaPos = ci::Vec3f(ci::Vec3f::zero());
+  
+  mBookPile = dowa::ResourceManager::texture().get(CinderellaTextureKey::BookPile);
+  mBookOpen = dowa::ResourceManager::texture().get(CinderellaTextureKey::BookOpen);
+  mBookSide = dowa::ResourceManager::texture().get(CinderellaTextureKey::BookSide);
   
   setColliderType(Collider::Rect);
 }
 
 void Book::setup() {}
 
-void Book::update() {}
+void Book::update()
+{
+  
+  mCinderellaPos = Task::find("Cinderella")->getPos();
+  
+  if(mCinderellaPos.x > 1068 || mFallFlag)
+  {
+    if(mRotate.z < 90)
+    {
+      mAcceleration += mGravityPower;
+      mRotate.z += mAcceleration;
+    }
+    else if (mBookName == mBookSideName) // onCo(ryで名前を変えると止まる
+    {
+      mAcceleration += mGravityPower2;
+      mPos.y -= mAcceleration;
+    }
+  }
+  
+}
 
 void Book::draw()
 {
@@ -39,9 +61,8 @@ void Book::draw()
   ci::gl::enable(GL_CULL_FACE);
   ci::gl::enable(GL_TEXTURE_2D);
   
-  if(mBookName == mBookBackName) drawBookBack();
-  if(mBookName == mBookDroppedName) drawBookDropped();
   if(mBookName == mBookPileName) drawBookPile();
+  if(mBookName == mBookSideName) drawBookSide();
   if(mBookName == mBookOpenName) drawBookOpen();
   
   ci::gl::disable(GL_CULL_FACE);
@@ -51,38 +72,42 @@ void Book::draw()
   ci::gl::popModelView();
 }
 
-void Book::drawBookBack()
-{
-  mBookBack.bind();
-  ci::gl::translate(mPos); // 移動させる
-  ci::gl::rotate(ci::Vec3f(180, 0, 0)); // 回転
-  ci::gl::drawCube(ci::Vec3f(0, 0, 0), mSize); // 本４
-  mBookBack.unbind();
-}
-
-void Book::drawBookDropped()
-{
-  mBookDropped.bind();
-  ci::gl::translate(mPos); // 移動させる
-  ci::gl::rotate(ci::Vec3f(180, 0, 0)); // 回転
-  ci::gl::drawCube(ci::Vec3f(0, 0, 0), mSize); // 本４
-  mBookDropped.unbind();
-}
-
 void Book::drawBookPile()
 {
   mBookPile.bind();
-  ci::gl::translate(mPos); // 移動させる
-  ci::gl::rotate(ci::Vec3f(180, 0, 0)); // 回転
-  ci::gl::drawCube(ci::Vec3f(0, 0, 0), mSize); // 本４
+  ci::gl::translate(mPos);
+  ci::gl::rotate(ci::Vec3f(180.f, 0.f, 0.f));
+  ci::gl::drawCube(ci::Vec3f(ci::Vec3f::zero()), mSize);
   mBookPile.unbind();
 }
 
 void Book::drawBookOpen()
 {
   mBookOpen.bind();
-  ci::gl::translate(mPos); // 移動させる
-  ci::gl::rotate(ci::Vec3f(180, 0, 0)); // 回転
-  ci::gl::drawCube(ci::Vec3f(0, 0, 0), mSize); // 本４
+  ci::gl::translate(mPos);
+  ci::gl::rotate(ci::Vec3f(180.f, 0.f, 0.f));
+  ci::gl::drawCube(ci::Vec3f(ci::Vec3f::zero()), mSize);
   mBookOpen.unbind();
+}
+
+void Book::drawBookSide()
+{
+  mBookSide.bind();
+  ci::gl::translate(mPos);
+  ci::gl::rotate(mRotate);
+  ci::gl::drawCube(ci::Vec3f(ci::Vec3f::zero()), mSize);
+  mBookSide.unbind();
+}
+
+void Book::onCollisionUpdate(const std::shared_ptr<Object>& compare)
+{
+  if(compare -> getName() == "Ball")
+  {
+    mFallFlag = true;
+  }
+  
+  if(compare -> getName() == "Floor_Floor")
+  {
+    mBookName = "BookOpen"; // 上のif文と組み合わせ注意
+  }
 }
