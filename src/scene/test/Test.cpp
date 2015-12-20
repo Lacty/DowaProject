8f439bdb1f19c09e705eff6c5461bffaf1da5a08
Lacty <akira206@gmail.com>
2015-12-21 06:10:47 +0900

    testコスイン

--- src/scene/test/Test.cpp
+++ src/scene/test/Test.cpp
@@ -5,29 +5,12 @@
 
 Test::Test() {
   std::cout << "start test" << std::endl;
-  
-  // リソースマネージャーに音をロードさせる
-  dowa::ResourceManager::audio().insert("sound/se/Hit.m4a", AudioKey::HitSE);
-  
-  // リソースマネージャーから音を取得
-  se = dowa::ResourceManager::audio().get(AudioKey::HitSE);
-  
-  // 音量をmaxに設定
-  se.gain->setValue(1.0f);
-}
+  }
 
 Test::~Test() {
   std::cout << "end test" << std::endl;
 }
 
-void Test::update() {
-  // タッチしたら
-  if (dowa::Device::isTouchBegan()) {
-    // 音を鳴らす
-    se.bgm->start();
-  }
-}
+void Test::update() {}
 
-void Test::draw() {
-
-}
+void Test::draw() {}
--- src/scene/test/Test.hpp
+++ src/scene/test/Test.hpp
@@ -1,12 +1,10 @@
 
 #pragma once
 #include "../Scene.hpp"
-#include "../../resource/ResourceManager.hpp"
 
 
 class Test : public Scene {
 private:
-  dowa::hidden::Audio se;
   
 public:
   Test();
