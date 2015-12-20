9391f72b977cb09573a8a0ff07ff77233dda6b76
Lacty <akira206@gmail.com>
2015-12-21 03:22:21 +0900

    Resource追加 AudioManager TextureManager ResKeyをinclude

---
+++ src/resource/Resource.cpp
@@ -0,0 +1,9 @@
+//
+//  Resource.cpp
+//  DowaProject
+//
+//  Created by Yanai on 2015/12/21.
+//
+//
+
+#include "Resource.hpp"
---
+++ src/resource/Resource.hpp
@@ -0,0 +1,5 @@
+
+#pragma once
+#include "AudioManager.hpp"
+#include "TextureManager.hpp"
+#include "ResKey.hpp"
