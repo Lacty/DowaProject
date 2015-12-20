9d53f9c050f5b5c49fbea0f0e92cbc19b9c99448
hiramasa <newdiamondsong1175@gmail.com>
2015-12-21 03:17:08 +0900

    Create Apple.hpp

---
+++ src/scene/cinderella/apple/Apple.hpp
@@ -0,0 +1,21 @@
+
+#pragma once
+
+#include "../../../object/Object.hpp"
+
+#include "cinder/gl/Texture.h"
+
+class Apple : public Object
+{
+  ci::gl::Texture mApple;
+  
+public:
+  
+  Apple(const ci::Vec3f&, const ci::Vec3f&);
+  
+  void setup();
+  void update();
+  void draw();
+  
+  void onCollisionUpdate(const std::shared_ptr<Object>& compare);
+};
