20565f64167389c9fd506933b35c86ee42a76906
Lacty <akira206@gmail.com>
2015-12-21 03:55:39 +0900

    resultload不必要になるかもしれないのでゴミ除去

--- src/scene/result/ResultLoad.cpp
+++ src/scene/result/ResultLoad.cpp
@@ -1,12 +1,9 @@
 
 #include "ResultLoad.hpp"
-#include "../../resource/ResourceManager.hpp"
 #include "../SceneManager.hpp"
 
 
-ResultLoad::ResultLoad() :
-//mLoad(&ResultLoad::load, this),
-isFin(false) {
+ResultLoad::ResultLoad() {
   std::cout << "start result load" << std::endl;
 }
 
@@ -16,25 +13,20 @@ ResultLoad::~ResultLoad() {
 
 void ResultLoad::load() {
   // Texture
-  dowa::ResourceManager::texture().insert("background/result/Back.jpg", TextureKey::Back);
-  dowa::ResourceManager::texture().insert("background/result/Retry.jpg", TextureKey::Retry);
-  dowa::ResourceManager::texture().insert("background/result/Select.jpg", TextureKey::Select);
-  dowa::ResourceManager::texture().insert("background/result/Fin.jpg", TextureKey::Fin);
-  dowa::ResourceManager::texture().insert("background/result/Font.png", TextureKey::Font);
+  // Back
+  // Retry
+  // Select
+  // Fin
+  // Font
 
   // Sound
-
-  isFin = true;
+  
+  std::cout << "finish load" << std::endl;
 }
 
 void ResultLoad::update() {
   load();
-  if (isFin) {
-    std::cout << "finish load" << std::endl;
-    //mLoad.join();
-    isFin = false;
-    SceneManager::create(SceneType::Result);
-  }
+  SceneManager::create(SceneType::Result);
 }
 
 void ResultLoad::draw() {}
--- src/scene/result/ResultLoad.hpp
+++ src/scene/result/ResultLoad.hpp
@@ -1,13 +1,10 @@
 
 #pragma once
 #include "../Scene.hpp"
-#include <thread>
 
 
 class ResultLoad : public Scene {
 private:
-  std::thread mLoad;
-  bool isFin;
   void load();
 
 public:
