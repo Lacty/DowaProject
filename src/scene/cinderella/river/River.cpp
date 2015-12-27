5ca1034312e4046b1871c50f20a63f1139d46eca
udontakoyaki <udontakoyaki22@yahoo.co.jp>
2015-12-28 00:12:56 +0900

    画像追加

---
+++ src/scene/cinderella/river/River.cpp
@@ -0,0 +1,9 @@
+//
+//  River.cpp
+//  DowaProject
+//
+//  Created by nanashi on 2015/12/27.
+//
+//
+
+#include "River.hpp"
---
+++ src/scene/cinderella/river/River.hpp
@@ -0,0 +1,14 @@
+//
+//  River.hpp
+//  DowaProject
+//
+//  Created by nanashi on 2015/12/27.
+//
+//
+
+#ifndef River_hpp
+#define River_hpp
+
+#include <stdio.h>
+
+#endif /* River_hpp */
